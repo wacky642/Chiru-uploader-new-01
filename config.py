12a2ebcfaee963d3fe6e64824634e04dbe9bc5cd
Chiru <hasenorc@gmail.com>
2025-03-29 17:32:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,25 @@
 import os
 
-API_ID = os.environ.get("API_ID", "26416404")
+API_ID = os.environ.get("API_ID", "")
 
-API_HASH = os.environ.get("API_HASH", "655b8dc1517dacb007deb53fc0fda30d")
+API_HASH = os.environ.get("API_HASH", "")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8014150927:AAEtzQnmFEPJQxSmcQEartgHJrySktbzWAA")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 7827463899))
+#OWNER = int(os.environ.get("OWNER", 7827463899))
 
-LOG = -1002159628443,
+#LOG = -1002159628443,
 
 # UPDATE_GRP = , # bot sat group
 
 # auth_chats = []
 
-try:
-    ADMINS=[7827463899]
-    for x in (os.environ.get("ADMINS", "7827463899").split()):
-        ADMINS.append(int(x))
-except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
-ADMINS.append(OWNER)
+#try:
+    #ADMINS=[7827463899]
+    #for x in (os.environ.get("ADMINS", "7827463899").split()):
+        #ADMINS.append(int(x))
+#except ValueError:
+        #raise Exception("Your Admins list does not contain valid integers.")
+#ADMINS.append(OWNER)
